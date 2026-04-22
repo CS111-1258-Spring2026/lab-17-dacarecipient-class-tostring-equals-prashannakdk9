1f3639b9e51af12c17bfe96ac664544f7e9d7f72
prashannakdk9 <prashannakhadka09@gmail.com>
2026-04-22 21:29:44 +0000

    changed

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
-/********************************************
-*	DACArecipient Tester
-*********************************************
-*	PROGRAM DESCRIPTION:
-*	This program is to test methods of the DACArecipient class.
-*********************************************/
-class Main 
-{
-  public static void main(String[] args) 
-	{
+class Main {
+  public static void main(String[] args) {
+    DACArecipient p1 = new DACArecipient();
+    p1.setAll("Mendez", "Javier", "56-3-445", "El Salvador", 2451564, 3956753, 3956840, 'M');
     
+    DACArecipient p2 = new DACArecipient();
+    p2.setAll("Mendez", "Javier", "56-3-445", "El Salvador", 2451564, 3956753, 3956840, 'M');
+
+    // Part 1: Test toString
+    System.out.println("--- toString() Test ---");
+    System.out.println(p1);
+
+    // Part 2: Test equals
+    System.out.println("\n--- equals() Test ---");
+    System.out.println("Are they equal? " + p1.equals(p2));
+
+    // Part 4: Test printCard
+    System.out.println("\n--- printCard() Test ---");
+    System.out.println(p1.printCard());
   }
 }
